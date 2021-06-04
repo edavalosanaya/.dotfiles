4c90158a16f9abb97fe3eb09ccbfed3d0e1ca7f6
edavalosanaya <davalosaeduardo@gmail.com>
2021-06-04 17:45:25 -0500

    Fixed typo in .xmodmaprc, relocated config to be insde i3, and added .fonts into the .dotfiles

--- sym_links.sh
+++ sym_links.sh
@@ -1,5 +1,13 @@
 #!/bin/bash 
 
-ln -sf $HOME/.dotfiles/config $HOME/.config/i3/config
-ln -sf $HOME/.dotfiles/.xmodmaprc $HOME/.xmodmaprc
-ln -sf $HOME/.dotfiles/.vimrc $HOME/.vimrc
+# i3 configuration
+ln -sf $HOME/.dotfiles/i3 $HOME/.config/ # directory
+
+# Keybinding configuration
+ln -sf $HOME/.dotfiles/.xmodmaprc $HOME/.xmodmaprc # file
+
+# vim configuration
+ln -sf $HOME/.dotfiles/.vimrc $HOME/.vimrc # file
+
+# fonts configuration 
+ln -sf $HOME/.dotfiles/.fonts $HOME/ # directory
