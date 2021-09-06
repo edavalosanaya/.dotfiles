1f0f06b8f9803c4ef3062343868282a5295ba6dc
edavalosana <davalosaeduardo@gmail.com>
2021-09-06 17:08:23 -0500

    fixed duplicating polybar instances and made keybindings.json work for different joplin configurations

--- sym_links.sh
+++ sym_links.sh
@@ -23,10 +23,16 @@ ln -sf $HOME/.dotfiles/gtk/gtk-3.0 $HOME/.config/ # directory
 ln -sf $HOME/.dotfiles/gtk/gtk-4.0 $HOME/.config/ # directory
 
 # joplin configuration
-ln -sf $HOME/.dotfiles/joplin/keymap.json $HOME/.config/joplin/keymap.json
+# Multiple possible locations
+if [ -d "$HOME/.config/joplin" ];
+then
+    ln -sf $HOME/.dotfiles/joplin/keymap.json $HOME/.config/joplin/keymap.json
+elif [ -d "$HOME/.config/joplin-desktop" ];
+then
+    ln -sf $HOME/.dotfiles/joplin/keymap.json $HOME/.config/joplin-desktop/keymap.json
+fi
 
 # invert script configuration
-if [ ! -f /usr/bin/invert-color ]; # if the link does not exist, create it
-	then
-		sudo ln -s $HOME/.dotfiles/invert.sh /usr/bin/invert-color
+if [ ! -f /usr/bin/invert-color ]; then # if the link does not exist, create it
+    sudo ln -s $HOME/.dotfiles/invert.sh /usr/bin/invert-color
 fi
